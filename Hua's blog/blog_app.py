cbc9da2d29dd8e99b0d356bff06f94e94d17f008
Huazui <1392728566@qq.com>
2024-08-06 12:15:30 +0800

    Update blog_app.py

--- Hua's blog/blog_app.py
+++ Hua's blog/blog_app.py
@@ -132,7 +132,7 @@ def delete_comment(post_id, comment_index):
         return "An error occurred", 500
 
 
+
 if __name__ == '__main__':
-    if not os.path.exists(UPLOAD_FOLDER):
-        os.makedirs(UPLOAD_FOLDER)
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
